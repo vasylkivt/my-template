228f47e3442b3350327f2a15d6974579d7b15104
Taras <117749635+vasylkivt@users.noreply.github.com>
2023-03-05 10:58:10 +0200

    add arch

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import 'animate.css';
+
+// import './js/slider';
+// import './js/gallery';
+// import './js/gallery-test';
+// import './js/slider-restaurant';
+// import './js/slider-restaurant-menu';
+
+
+
+// import './js/mobile-menu';
+// import './js/modal';
+
+
+import './js/scroll-header-btn-top';
+import './js/anchor-scroll';
+
+
+
+
